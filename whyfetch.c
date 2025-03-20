bce34c469bdf7182d7f00145f451b959f145ee84
emzilia <em@zilia.dev>
2025-03-20 17:59:11 -0400

    simplified username/host/shell funcs, fully functional

--- whyfetch.c
+++ whyfetch.c
@@ -1,67 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pwd.h>
+
+char *duck1 = "\n       ,~~.	   Who: ";
+char *duck2 = "  ,   (  - )>	    OS: ";
+char *duck3 = "  )`~~'   (	Kernel: ";
+char *duck4 = " (  .__)   )	 Shell: ";
+char *duck5 = "  `-.____,' 	\n";
 
 char *get_username(void)
 {
-	const int bufferSize = 32;
-	char buffer[bufferSize];
-	char *result = NULL;
-	size_t resultSize = 0;
-
-	FILE *p = popen("whoami", "r");
-	if (p == NULL) return NULL;
-	
-	while (fgets(buffer, bufferSize, p) != NULL) {
-		size_t fragmentSize = strlen(buffer);
-		char *newResult = realloc(
-			result, resultSize + fragmentSize + 1
-		);
-		result = newResult;
-		memcpy(result + resultSize, buffer, fragmentSize);
-		resultSize += fragmentSize;
-	}
-
-	int status = pclose(p);
-	if (status == -1) free(result);
-
-	char*finalResult = realloc(result, resultSize + 1);
-	finalResult[resultSize] = '\0';
-
-	finalResult[strcspn(finalResult, "\n")] = 0;
-
-	return finalResult;
+	char *username = getenv("USER");
+	return username;
 }
 
 char *get_hostname(void)
 {
-	const int bufferSize = 32;
-	char buffer[bufferSize];
-	char *result = NULL;
-	size_t resultSize = 0;
-
-	FILE *p = popen("hostname", "r");
-	if (p == NULL) return NULL;
-	
-	while (fgets(buffer, bufferSize, p) != NULL) {
-		size_t fragmentSize = strlen(buffer);
-		char *newResult = realloc(
-			result, resultSize + fragmentSize + 1
-		);
-		result = newResult;
-		memcpy(result + resultSize, buffer, fragmentSize);
-		resultSize += fragmentSize;
-	}
-
-	int status = pclose(p);
-	if (status == -1) free(result);
-
-	char*finalResult = realloc(result, resultSize + 1);
-	finalResult[resultSize] = '\0';
-
-	finalResult[strcspn(finalResult, "\n")] = 0;
-
-	return finalResult;
+	char *hostname = getenv("HOSTNAME");
+	return hostname;
 }
 
 char *get_prettyname(void)
@@ -98,13 +55,49 @@ char *get_prettyname(void)
 
 	return finalResult;
 }
+char *get_shell(void)
+{
+    char *shell = getenv("SHELL");
+    return shell;
+} 
+
+char *get_kernelv(void)
+{
+	const int bufferSize = 32;
+	char buffer[bufferSize];
+	char *result = NULL;
+	size_t resultSize = 0;
+
+	FILE *p = popen("uname -r", "r");
+	if (p == NULL) return NULL;
+	
+	while (fgets(buffer, bufferSize, p) != NULL) {
+		size_t fragmentSize = strlen(buffer);
+		char *newResult = realloc(
+			result, resultSize + fragmentSize + 1
+		);
+		result = newResult;
+		memcpy(result + resultSize, buffer, fragmentSize);
+		resultSize += fragmentSize;
+	}
+
+	int status = pclose(p);
+	if (status == -1) free(result);
+
+	char*finalResult = realloc(result, resultSize + 1);
+	finalResult[resultSize] = '\0';
+
+	finalResult[strcspn(finalResult, "\n")] = 0;
+
+	return finalResult;
+}
 
 char *combine_userhost(char *username, char *hostname)
 {
 	const char *atsign = "@";
 
 	size_t resultSize = (
-		strlen(username) + strlen(atsign) + strlen(hostname) + 1
+		strlen(username) + 1 + strlen(hostname) + 1
 	);
 
 	char *result = (char*)malloc(resultSize);
@@ -114,13 +107,41 @@ char *combine_userhost(char *username, char *hostname)
 	return result;
 }
 
+char *combine_ascii(char* ascii, char* data)
+{
+	size_t resultSize = (
+		strlen(ascii) + strlen(data) + 1
+	);
+
+	char *resultDuck = (char*)malloc(resultSize);
+
+	sprintf(resultDuck, "%s%s", ascii, data);
+
+	return resultDuck;
+
+}
+
 int main(void)
 {
 	char *username = get_username();
 	char *hostname = get_hostname();
+
 	char *userhost = combine_userhost(username, hostname);
 	char *prettyname = get_prettyname();
+	char *kernelv = get_kernelv();
+	char *usershell = get_shell();
+
+	char *resultDuck1 = combine_ascii(duck1, userhost);
+	char *resultDuck2 = combine_ascii(duck2, prettyname);
+	char *resultDuck3 = combine_ascii(duck3, kernelv);
+	char *resultDuck4 = combine_ascii(duck4, usershell);
+	//char *resultDuck5 = combine_ascii(duck1, userhost);
 	
-	printf("%s\n%s\n", userhost, prettyname);
+	
+	printf(
+		"%s\n%s\n%s\n%s\n%s\n",
+		resultDuck1, resultDuck2, resultDuck3,
+		resultDuck4, duck5
+	);
 
 }
